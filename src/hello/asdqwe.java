f4275fb10d4721c8257061d14a6983afb11a9a7b
sunghun4118 <65937628+sunghun4118@users.noreply.github.com>
2020-05-26 15:04:42 +0900

    Add files via upload

---
+++ src/hello/asdqwe.java
@@ -0,0 +1,13 @@
+package hello;
+
+public class asdqwe {
+	public static void main(String[] args) {
+		System.out.println("hi! java");
+		System.out.println("hi! python");
+		System.out.println("hi! javascript");
+		System.out.println("hi! javascript");
+		System.out.println("hi! javascript");
+		
+	}
+
+}
